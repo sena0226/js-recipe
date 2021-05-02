abec6d9a01b8eeaea799c3c616f7048df54c7486
sena0226 <ayrton2_02.26@au.com>
2021-05-02 18:40:52 +0900

    zihanki

---
+++ 3_semi/week1/object.js
@@ -0,0 +1,16 @@
+const sena = {
+  name: "荒川誓那",
+  university: "立教大学",
+  hobby: ["テニス", "読書", "ダーツ"],
+  sayhello: function() {
+    console.log("こんにちは")
+  },
+  isGamementor: true,
+  isExmentor: false,
+  senaFamily: {
+    father: "kiyotaka",
+    mother: "yumiko",
+    sister: "rira",
+    brother: "ayato",
+  },
+}
